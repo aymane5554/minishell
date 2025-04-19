ea6e35d03138d394b2fea2ea8484ab1db7d30af3
Taoufiq Ibariken <tibarike@e1r7p13.1337.ma>
2025-04-19 10:55:52 +0100

    ...¨

--- main.c
+++ main.c
@@ -3,22 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayel-arr <ayel-arr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tibarike <tibarike@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 13:30:40 by tibarike          #+#    #+#             */
-/*   Updated: 2025/04/17 16:53:27 by ayel-arr         ###   ########.fr       */
+/*   Updated: 2025/04/19 10:53:01 by tibarike         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+int	ft_dstrlen(char **str)
+{
+	int	i;
+	i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
 int main(void)
 {
 	int		i;
 	int		j;
+	int		cmds_len;
 	char	**cmds;
 	char	**cmd;
 	char	*tmp;
+	t_cmd	**all_cmds;
 
 	while (1)
 	{
@@ -27,10 +40,11 @@ int main(void)
 		if (!validate_input(line))
 			continue;
 		cmds = ft_split_pipe(line, '|');
+		cmds_len = ft_dstrlen(cmds);
+		all_cmds = malloc(sizeof(t_cmd) * cmds_len);
 		i = 0;
 		while (cmds[i])
 		{
-			printf("input : %s\n", cmds[i]);
 			if (ft_strchr(cmds[i], '>') || ft_strchr(cmds[i], '<'))
 			{
 				tmp = cmds[i];
--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayel-arr <ayel-arr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tibarike <tibarike@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:12:13 by tibarike          #+#    #+#             */
-/*   Updated: 2025/04/17 16:20:48 by ayel-arr         ###   ########.fr       */
+/*   Updated: 2025/04/19 10:41:03 by tibarike         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,17 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-typedef struct s_token
+typedef struct s_redr
 {
-}	t_token;
+	char	*file;
+	int		type; //0 out, 1 in, 3 append, 4 herdoc
+}	t_redr;
+
+typedef struct s_cmd
+{
+	char	**cmd;
+	t_redr	redirection;
+}	t_cmd;
 
 int		valid_quotes(char	*str);
 bool	validate_input(char *input);
