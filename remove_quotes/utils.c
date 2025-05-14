39813d40c97ed3629622ac9f60c790a34d5a7273
Aymen El Arrouji <aymane.arrouji@gmail.com>
2025-05-14 10:50:12 +0100

    quotes fix

--- remove_quotes/utils.c
+++ remove_quotes/utils.c
@@ -6,40 +6,40 @@
 /*   By: ayel-arr <ayel-arr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:16:29 by ayel-arr          #+#    #+#             */
-/*   Updated: 2025/05/13 16:30:16 by ayel-arr         ###   ########.fr       */
+/*   Updated: 2025/05/14 10:49:21 by ayel-arr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	should_i_skip(char c, char quote, int *i)
+static char	should_i_skip(char c, char *quote, int *i)
 {
-	if (c == '\'' && quote == 0)
+	if (c == '\'' && *quote == 0)
 	{
-		quote = 1;
+		*quote = 1;
 		(*i)++;
 		return (1);
 	}
-	else if (c == '\'' && quote == 1)
+	else if (c == '\'' && *quote == 1)
 	{
-		quote = 0;
+		*quote = 0;
 		(*i)++;
 		return (1);
 	}
 	return (0);
 }
 
-static char	should_i_skip2(char c, char quote, int *i)
+static char	should_i_skip2(char c, char *quote, int *i)
 {
-	if (c == '\"' && quote == 0)
+	if (c == '\"' && *quote == 0)
 	{
-		quote = 2;
+		*quote = 2;
 		(*i)++;
 		return (1);
 	}
-	else if (c == '\"' && quote == 2)
+	else if (c == '\"' && *quote == 2)
 	{
-		quote = 0;
+		*quote = 0;
 		(*i)++;
 		return (1);
 	}
@@ -57,9 +57,9 @@ static int	size_without_quotes(char *s)
 	quote = 0;
 	while (s[i])
 	{
-		if (should_i_skip(s[i], quote, &i))
+		if (should_i_skip(s[i], &quote, &i))
 			continue ;
-		else if (should_i_skip2(s[i], quote, &i))
+		else if (should_i_skip2(s[i], &quote, &i))
 			continue ;
 		i++;
 		j++;
@@ -82,9 +82,9 @@ int	remove_quotes(char **cmds, char *str, int n)
 		return (1);
 	while (str[i])
 	{
-		if (should_i_skip(str[i], quote, &i))
+		if (should_i_skip(str[i], &quote, &i))
 			continue ;
-		else if (should_i_skip2(str[i], quote, &i))
+		else if (should_i_skip2(str[i], &quote, &i))
 			continue ;
 		new[j] = str[i];
 		i++;
